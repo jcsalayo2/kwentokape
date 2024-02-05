9bff1a6a1b56c6ecbb96fabaae7c48ea898cbf84
jcsalayo2 <116143080+jcsalayo2@users.noreply.github.com>
2024-02-06 02:17:57 +0800

    add intro page

---
+++ lib/constants/constants.dart
@@ -0,0 +1,4 @@
+String introduction =
+    "Welcome to KwentoKape, where conversations spark and stories flourish! Unlike your typical chat app, KwentoKape connects you with strangers globally, offering a space to share and discover compelling narratives.\n\n"
+    "More than just a chat platform, KwentoKape is about weaving a tapestry of human experiences. It's not about coffee; it's about connecting with diverse individuals, breaking the barriers of routine, and fostering genuine connections.\n\n"
+    "Explore the magic of anonymous conversations, where every chat is an opportunity for inspiration, fresh perspectives, and meaningful encounters. Download KwentoKape now and dive into a world where strangers become storytellers, creating a unique blend of shared narratives. Cheers to the art of connection!";
